389085af666cc974aabaa9b5f8b6ef90b471ab81
diegorufe <diegorufe@gmail.com>
2020-02-15 20:50:53 +0100

    Export database config with sequelize

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 const EXPRESS = require("./lib/express.config");
 // Services and beans services for application
 const SERVICES = require("./lib/service.config");
+// Configuration for database sequelize
+const DATABASE_SEQUELIZE = require("./lib/database.sequelize");
 
 module.exports = {
   rfnoderest: {
     create_express_app: EXPRESS.create_express_app,
     status_htpp: EXPRESS.status_htpp,
-    services: SERVICES
+    services: SERVICES,
+    databaseSequelize: DATABASE_SEQUELIZE
   }
 };
