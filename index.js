f0f1c3147ed5f9c4b6897caccca38247180c296d
diegorufe <diegorufe@gmail.com>
2020-10-11 18:07:18 +0200

    Added filter only and expresion, order, and limit

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const EXPRESS = require("./lib/express.config");
 const SERVICES = require("./lib/service.config");
 // Configuration for database sequelize
 const DATABASE_SEQUELIZE = require("./lib/database.sequelize");
+// Configuration for database mongo
+const DATABASE_MONGO = require("./lib/database.mongodb");
 
 module.exports = {
   rfnoderest: {
     create_express_app: EXPRESS.create_express_app,
     status_htpp: EXPRESS.status_htpp,
     services: SERVICES,
-    databaseSequelize: DATABASE_SEQUELIZE
-  }
+    databaseSequelize: DATABASE_SEQUELIZE,
+    databaseMongo: DATABASE_MONGO,
+  },
 };
